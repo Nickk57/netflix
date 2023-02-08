b8e3156c5b652d141e8ded379c3e3aaf1cc01928
nicolas guichard <guichard.nicolas57@gmail.com>
2023-02-08 16:24:49 +0100

    include

---
+++ include/header.php
@@ -0,0 +1,28 @@
+<div class="container-fluid text-center">
+    <div class="row justify-content-between">
+        <div class="row-2"><img src="images/Netflix_Logo_PMS.png" alt="logo" height="75"></div>
+        <div class="row-4">
+            <ul class="nav">
+                <li class="nav-item"><a href="#">Accueil</a></li>
+                <li class="nav-item"><a href="#">Séries</a></li>
+                <li class="nav-item"><a href="#">Films</a></li>
+                <li class="nav-item"><a href="#">Nouveautés</a></li>
+                <li class="nav-item"><a href="#">les plus regardées</a></li>
+                <li class="nav-item"><a href="#">Ma liste</a></li>
+                <li class="nav-item"><a href="#">Explorer par langue</a></li>
+            </ul>
+        </div>
+        <div class="row-4">
+            <ul class="nav">
+                <li class="nav-item">
+                    <input class="form-control" type="search" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><img src="images/42-search-outline-white.gif" alt="recherche" width="16" height="16"></button>
+                </li>
+                <li class="nav-item"><a href="#">DIRECT</a></li>
+                <li class="nav-item"><a href="#">Jeunesse</a></li>
+                <li class="nav-item"><a href="#"><img src="images/46-notification-bell-outline-white.gif" height="50" alt="cloche"></a></li>
+                <li class="nav-item"><a href="#"><img src="images/8-account-outline-white.gif" height="50" alt="compte"></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
