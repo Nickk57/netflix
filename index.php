e7da6782c3edeb0170b9a83319d67498eec58c73
nicolas guichard <guichard.nicolas57@gmail.com>
2023-02-08 16:18:29 +0100

    tout

--- index.php
+++ index.php
@@ -11,7 +11,161 @@
 <body>
     <header><?php include('include/header.php') ?></header>
     <main>
-
+        <div class="col">
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                    </div>
+                </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            <div id="carouselExample2" class="carousel slide">
+                <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                    </div>
+                </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample2" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample2" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            <div id="carouselExample3" class="carousel slide">
+                <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                        <img src="images/films/saw.jpg" class="d-block img">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                        <img src="images/films/chuky.jpg" class="d-block img">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                        <img src="images/films/léon.jpg" class="d-block img">
+                    </div>
+                </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample3" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample3" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
