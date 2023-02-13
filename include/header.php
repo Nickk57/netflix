0af847071791e4327aba060999de3cebba918e5d
nicolas guichard <guichard.nicolas57@gmail.com>
2023-02-13 10:53:33 +0100

    tout

--- include/header.php
+++ include/header.php
@@ -1,28 +1,41 @@
-<div class="container-fluid text-center">
-    <div class="row justify-content-between">
-        <div class="row-2"><img src="images/Netflix_Logo_PMS.png" alt="logo" height="75"></div>
-        <div class="row-4">
-            <ul class="nav">
-                <li class="nav-item"><a href="#">Accueil</a></li>
-                <li class="nav-item"><a href="#">Séries</a></li>
-                <li class="nav-item"><a href="#">Films</a></li>
-                <li class="nav-item"><a href="#">Nouveautés</a></li>
-                <li class="nav-item"><a href="#">les plus regardées</a></li>
-                <li class="nav-item"><a href="#">Ma liste</a></li>
-                <li class="nav-item"><a href="#">Explorer par langue</a></li>
-            </ul>
-        </div>
-        <div class="row-4">
-            <ul class="nav">
-                <li class="nav-item">
-                    <input class="form-control" type="search" aria-label="Recipient's username" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><img src="images/42-search-outline-white.gif" alt="recherche" width="16" height="16"></button>
-                </li>
-                <li class="nav-item"><a href="#">DIRECT</a></li>
-                <li class="nav-item"><a href="#">Jeunesse</a></li>
-                <li class="nav-item"><a href="#"><img src="images/46-notification-bell-outline-white.gif" height="50" alt="cloche"></a></li>
-                <li class="nav-item"><a href="#"><img src="images/8-account-outline-white.gif" height="50" alt="compte"></a></li>
-            </ul>
-        </div>
+<nav class="navbar navbar-expand-lg text-white bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="images/Netflix_Logo_PMS.png" alt="logo" height="75"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse text-white " id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-white ">
+        <li class="nav-item text-white ">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Séries</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Films</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Nouveautés</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">les plus regardées</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Ma liste</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Explorer par langue</a>
+        </li>
+      </ul>
+      <form class="d-flex align-bottom" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-dark" type="submit"><img src="images/42-search-outline-white.gif" alt="recherche" width="16" height="16"></button>
+        <a href="#">DIRECT</a></li>
+        <a href="#">Jeunesse</a></li>
+        <button class="btn btn-dark" type="submit"><a href="#"><img src="images/46-notification-bell-outline-white.gif" height="50" alt="cloche"></a></button>
+        <button class="btn btn-dark" type="submit"><a href="#"><img src="images/8-account-outline-white.gif" height="50" alt="compte"></a></button>
+      </form>
     </div>
-</div>
+  </div>
+</nav>
